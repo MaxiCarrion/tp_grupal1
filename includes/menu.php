c64e75acf5a8b93f32bce0b80ca52062da30ba26
Richard Campos <richardsjcb@gmail.com>
2021-11-01 22:42:56 -0300

    Se elimina la carpeta grupal1 y se pone en el folio prinicipal

---
+++ includes/menu.php
@@ -0,0 +1,23 @@
+<?php
+
+function menu(){ ?>
+
+<div class="fondo">
+    <header class="color: white">
+        <div class="d-flex justify-content-end" style="margin-right: 3rem">
+            <nav class="navegacion" >
+                
+                <a style="color: white" target="_blank" href="https://www.instagram.com/sabry_congela/">Instagram</a>
+                <a style="color: white; margin: 2rem" target="_blank" href="https://www.facebook.com/sabry_congela-107748967639791/">Facebook</a>
+                <a style= "color: white; margin: 2rem" target="_blanck" href="login.php">Ingresar</a>
+            </nav>
+        </div>
+
+             
+    </header>
+</div>
+
+<?php
+}
+
+?>
